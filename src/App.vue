3b83ebf5d36a7dc2f4d5692302162806453370e5
Iza-a <161318041+Iza-a@users.noreply.github.com>
2024-09-30 09:05:55 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <nav>
+      <ul>
+      <router-link to="/">Home</router-link>
+      <router-link to="/videos">Videos Diversos</router-link>
+    </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+  <div id="app">
+    <HelloWorld />
+  </div>
+  <div id="app">
+    <TabelaDiversos />
+  </div>
+  
+</template>
+
+<script>
+import HelloWorld from './components/PaginaPrincipal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+
+
+</script>
+
+<style>
+
+nav{
+  width: 100%;
+  background: rgb(75, 88, 202);
+  margin: 0%
+}
+
+ul{
+  display: flex;
+  padding: 2rem;
+  gap: 1rem;
+  text-decoration: none;
+  cursor: pointer !important;
+}
+
+a{
+  text-decoration: none;
+  color: blue;
+}
+
+</style>
